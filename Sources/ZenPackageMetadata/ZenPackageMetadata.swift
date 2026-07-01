ad960cbadfa399bf8da05ff853e71b8bf0e97a20
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2026-07-01 08:24:37 +0200

    v1.0.0

--- Sources/ZenPackageMetadata/ZenPackageMetadata.swift
+++ Sources/ZenPackageMetadata/ZenPackageMetadata.swift
@@ -8,7 +8,7 @@
 public enum ZenPackageMetadata {
     public static let packageName = "ZenCODE"
     public static let coderExecutableName = "zen"
-    public static let version = "0.1.0"
+    public static let version = "1.0.0"
 
     public static func versionDescription(for executableName: String) -> String {
         "\(executableName) \(version)"
